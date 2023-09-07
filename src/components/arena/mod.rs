972f7425edca4eb253cc70629afcd2dbad194840
josh015 <joshua.d.ols@gmail.com>
2023-09-07 08:43:43 -0400

    Moved and renamed `spawn_arena` to `arena` module

--- src/components/arena/mod.rs
+++ src/components/arena/mod.rs
@@ -10,10 +10,204 @@ pub use ui::*;
 
 pub const ARENA_CENTER_POINT: Vec3 = Vec3::ZERO;
 
+/// Handles setting up the static arena entities.
+fn spawn_arena(
+    mut run_state: ResMut<RunState>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut spawn_wall_events: EventWriter<SpawnWallEvent>,
+) {
+    // Cameras
+    commands.spawn((SwayingCamera, Camera3dBundle::default()));
+
+    // Light
+    let light_transform = Mat4::from_euler(
+        EulerRot::ZYX,
+        0.0,
+        std::f32::consts::FRAC_PI_4,
+        -std::f32::consts::FRAC_PI_4,
+    );
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            illuminance: 10000.0,
+            // shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_matrix(light_transform),
+        ..default()
+    });
+
+    // Ocean
+    commands.spawn((
+        AnimatedWater::default(),
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Plane {
+                size: 100.0,
+                subdivisions: 1,
+            })),
+            material: materials.add(StandardMaterial {
+                base_color: Color::hex("257AFFCC").unwrap(),
+                alpha_mode: AlphaMode::Blend,
+                ..default()
+            }),
+            transform: Transform::from_xyz(0.0, -0.01, 0.0),
+            ..default()
+        },
+    ));
+
+    // Beach
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane {
+            size: 1.0,
+            subdivisions: 1,
+        })),
+        material: materials.add(Color::hex("C4BD99").unwrap().into()),
+        transform: Transform::from_matrix(
+            Mat4::from_scale_rotation_translation(
+                Vec3::splat(GOAL_WIDTH),
+                Quat::IDENTITY,
+                ARENA_CENTER_POINT,
+            ),
+        ),
+        ..default()
+    });
+
+    // Goals
+    let unit_cube = meshes.add(Mesh::from(shape::Cube { size: 1.0 }));
+    let barrier_material = materials.add(Color::hex("750000").unwrap().into());
+    let goal_configs = [
+        (
+            2,
+            Side::Top,
+            Style {
+                align_self: AlignSelf::FlexEnd,
+                position_type: PositionType::Absolute,
+                justify_content: JustifyContent::Center,
+                top: Val::Px(5.0),
+                left: Val::Px(400.0),
+                ..default()
+            },
+        ),
+        (
+            1,
+            Side::Right,
+            Style {
+                align_self: AlignSelf::FlexEnd,
+                position_type: PositionType::Absolute,
+                justify_content: JustifyContent::Center,
+                top: Val::Px(400.0),
+                right: Val::Px(5.0),
+                ..default()
+            },
+        ),
+        (
+            0,
+            Side::Bottom,
+            Style {
+                align_self: AlignSelf::FlexEnd,
+                position_type: PositionType::Absolute,
+                justify_content: JustifyContent::Center,
+                bottom: Val::Px(5.0),
+                right: Val::Px(400.0),
+                ..default()
+            },
+        ),
+        (
+            3,
+            Side::Left,
+            Style {
+                align_self: AlignSelf::FlexEnd,
+                position_type: PositionType::Absolute,
+                justify_content: JustifyContent::Center,
+                bottom: Val::Px(400.0),
+                left: Val::Px(5.0),
+                ..default()
+            },
+        ),
+    ];
+
+    for (i, side, style) in goal_configs.iter() {
+        // Walls
+        spawn_wall_events.send(SpawnWallEvent {
+            side: *side,
+            is_instant: true,
+        });
+
+        // Goals
+        commands
+            .spawn((
+                *side,
+                Goal,
+                PbrBundle {
+                    transform: Transform::from_rotation(Quat::from_axis_angle(
+                        Vec3::Y,
+                        std::f32::consts::TAU
+                            * (*i as f32 / goal_configs.len() as f32),
+                    ))
+                    .mul_transform(Transform::from_xyz(
+                        0.0,
+                        0.0,
+                        GOAL_HALF_WIDTH,
+                    )),
+                    ..default()
+                },
+            ))
+            .with_children(|parent| {
+                // Barrier
+                parent.spawn((
+                    Barrier,
+                    Collider,
+                    PbrBundle {
+                        mesh: unit_cube.clone(),
+                        material: barrier_material.clone(),
+                        transform: Transform::from_matrix(
+                            Mat4::from_scale_rotation_translation(
+                                Vec3::new(
+                                    BARRIER_DIAMETER,
+                                    BARRIER_HEIGHT,
+                                    BARRIER_DIAMETER,
+                                ),
+                                Quat::IDENTITY,
+                                Vec3::new(
+                                    GOAL_HALF_WIDTH,
+                                    0.5 * BARRIER_HEIGHT,
+                                    0.0,
+                                ),
+                            ),
+                        ),
+                        ..default()
+                    },
+                ));
+            });
+
+        // Score
+        commands.spawn((
+            HitPointsUi,
+            *side,
+            TextBundle {
+                style: style.clone(),
+                text: Text::from_section(
+                    "",
+                    TextStyle {
+                        font: run_state.font_handle.clone(),
+                        font_size: 50.0,
+                        color: Color::RED,
+                    },
+                ),
+                ..default()
+            },
+        ));
+
+        run_state.goals_hit_points.insert(*side, 0);
+    }
+}
+
 pub struct ArenaPlugin;
 
 impl Plugin for ArenaPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((AnimatedWaterPlugin, SwayingCameraPlugin, UiPlugin));
+        app.add_plugins((AnimatedWaterPlugin, SwayingCameraPlugin, UiPlugin))
+            .add_systems(Startup, spawn_arena);
     }
 }
