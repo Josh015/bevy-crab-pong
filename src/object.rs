7ffab2dc0e3ce1ca7f8947ad8be68092fd65537b
josh015 <joshua.d.ols@gmail.com>
2023-09-15 18:04:22 -0400

    Code cleanup.

--- src/object.rs
+++ src/object.rs
@@ -32,13 +32,12 @@ impl Plugin for ObjectPlugin {
             (Object::Wall, spawn_wall_on_side),
             (Object::Crab, spawn_crab_on_side),
         ))
-        .add_plugins(SpewPlugin::<Object>::default())
         .add_plugins(SpewPlugin::<Object, Side>::default());
     }
 }
 
 fn spawn_wall_on_side(
-    In(goal_side): In<Side>,
+    In(side): In<Side>,
     cached_assets: Res<CachedAssets>,
     mut commands: Commands,
     beach: Option<Res<Beach>>,
@@ -46,14 +45,14 @@ fn spawn_wall_on_side(
 ) {
     let (goal_entity, _) = goals_query
         .iter()
-        .find(|(_, side)| **side == goal_side)
+        .find(|(_, goal_side)| **goal_side == side)
         .unwrap();
 
     commands.entity(goal_entity).with_children(|builder| {
         builder.spawn((
             Wall,
             Collider,
-            goal_side,
+            side,
             FadeBundle {
                 fade_animation: FadeAnimation::Scale {
                     max_scale: WALL_SCALE,
@@ -83,11 +82,11 @@ fn spawn_wall_on_side(
         ));
     });
 
-    info!("Wall({:?}): Spawned", goal_side);
+    info!("Wall({:?}): Spawned", side);
 }
 
 fn spawn_crab_on_side(
-    In(goal_side): In<Side>,
+    In(side): In<Side>,
     cached_assets: Res<CachedAssets>,
     game_mode: Res<GameMode>,
     game_assets: Res<GameAssets>,
@@ -97,7 +96,7 @@ fn spawn_crab_on_side(
     goals_query: Query<(Entity, &Side), With<Goal>>,
 ) {
     let game_config = game_configs.get(&game_assets.game_config).unwrap();
-    let crab_config = &game_config.modes[game_mode.0].competitors[&goal_side];
+    let crab_config = &game_config.modes[game_mode.0].competitors[&side];
     let material_handle = materials.add(StandardMaterial {
         base_color_texture: Some(game_assets.image_crab.clone()),
         base_color: Color::hex(&crab_config.color).unwrap(),
@@ -105,14 +104,14 @@ fn spawn_crab_on_side(
     });
     let (goal_entity, _) = goals_query
         .iter()
-        .find(|(_, side)| **side == goal_side)
+        .find(|(_, goal_side)| **goal_side == side)
         .unwrap();
 
     commands.entity(goal_entity).with_children(|builder| {
         let mut crab = builder.spawn((
             Crab,
             Collider,
-            goal_side,
+            side,
             FadeBundle {
                 fade_animation: FadeAnimation::Scale {
                     max_scale: CRAB_SCALE,
@@ -153,5 +152,5 @@ fn spawn_crab_on_side(
         }
     });
 
-    info!("Crab({:?}): Spawned", goal_side);
+    info!("Crab({:?}): Spawned", side);
 }
