93be63332ae19243a383bd6b68a2b9cdf3e39c78
josh015 <joshua.d.ols@gmail.com>
2025-03-17 21:21:35 -0400

    More specific window settings.

--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,27 @@ fn main() {
                 .set(WindowPlugin {
                     primary_window: Some(Window {
                         title: "Bevy Crab Pong".to_owned(),
-                        resolution: WindowResolution::new(800.0, 800.0),
                         present_mode: PresentMode::AutoVsync,
+                        position: WindowPosition::Centered(
+                            MonitorSelection::Primary,
+                        ),
+                        resolution: WindowResolution::new(640.0, 640.0),
+                        resize_constraints: WindowResizeConstraints {
+                            min_height: 640.0,
+                            min_width: 640.0,
+                            ..default()
+                        },
+                        // resizable: false,
+                        // enabled_buttons: EnabledButtons {
+                        //     maximize: false,
+                        //     ..default()
+                        // },
+                        fit_canvas_to_parent: true,
+                        // #[cfg(not(debug_assertions))]
+                        // canvas: Some("#pico-td".into()),
+                        // window_theme: Some(WindowTheme::Dark),
+                        // #[cfg(not(target_os = "windows"))]
+                        // visible: false,
                         ..default()
                     }),
                     ..default()
